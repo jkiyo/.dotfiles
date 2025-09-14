9a3e46975b63deb220849aa7943ea5c6bca617d9
Joao Kiyoshi <joaokiyo@gmail.com>
2025-09-13 23:48:34 -0300

    Add waybar rofi and updates on install scripts

--- install/user.sh
+++ install/user.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 install_packages() {
     sudo pacman -Syu
     sudo pacman -S \
+	hyprpaper \
+	keepassxc \
+	rofi \
+	waybar \
         git \
         hyprland \
         hyprpolkitagent \
@@ -18,16 +22,31 @@ install_packages() {
         qt6-wayland  \
         uwsm \
         wireplumber \
+	pipewire-pulse \
+	pipewire-alsa \
         xdg-desktop-portal-hyprland
 }
 
-setup_shell_profile() {
+create_link() {
+    echo "Linking $1 -> $2"
+    mkdir -p "$(basename $2)"
+    ln -sf "$1" "$2"
+}
+
+setup_dotfiles() {
     echo "Removing ~/.bash_profile, ~/.zsh, and ~/.profile"
     rm -f ~/.bash_profile
     rm -f ~/.profile
-    echo "Linking ~/.profile from ~/.dotfiles/.profile"
-    ln -s ~/.dotfiles/.profile ~/.profile
+
+    create_link ~/.dotfiles/.profile ~/.profile
+    create_link ~/.dotfiles/systemd ~/.config/systemd/user
+    create_link ~/.dotfiles/ssh ~/.ssh/config
+    create_link ~/.dotfiles/environment.d ~/.config/environment.d
+    create_link ~/.dotfiles/hypr ~/.config/hypr
+    create_link ~/.dotfiles/kitty ~/.config/kitty
+    create_link ~/.dotfiles/waybar ~/.config/waybar
+    create_link ~/.dotfiles/rofi ~/.config/rofi
 }
 
 install_packages
-setup_shell_profile
+setup_dotfiles
